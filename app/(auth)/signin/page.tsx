612eb6e0c0838f47c011667fefa19b2a6342fc72
abhishek-8081 <abhishekrajputji2004@gmail.com>
2025-03-28 15:12:06 +0530

    Signin and Signup

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,9 @@
+export default function signin(){
+    return <div>
+        Signin
+        <input type="text"></input>
+        <input type="text"></input>
+        <button>Signin</button>
+
+    </div>
+}
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function signup(){
+    return <div>
+        Signup
+        <input type="text"></input>
+        <input type="text"></input>
+        <button>Signup</button>
+
+    </div>
+}
